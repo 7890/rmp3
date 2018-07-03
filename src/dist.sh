70fbef3781861d75f063fc88554ebd0f4ec9cd1c
Thomas Brand <tom@trellis.ch>
2018-07-03 02:06:01 +0200

    run from directory where script is stored. add tools check. compile java class if not available. minimal error handling.

--- src/dist.sh
+++ src/dist.sh
@@ -1,23 +1,52 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #run in src directory
+cd "$DIR"
+
+checkAvail()
+{
+	which "$1" >/dev/null 2>&1
+	ret=$?
+	if [ $ret -ne 0 ]
+	then
+		echo "tool \"$1\" not found. please install"
+		exit 1
+	fi
+}
+
+
+for tool in java javac xmlstarlet; \
+	do checkAvail "$tool"; done
 
 if [ ! -r "mixxxml.class" ]
 then
 	echo -e "no mixxxml class found."
-	echo -e "compile with 'javac mixxxml.java'"
+	echo -e "compiling with 'javac mixxxml.java' ..."
+	javac mixxxml.java
+fi
+
+echo "generating xml and js file for rmp3 controller ..."
+java mixxxml controller.js | xmlstarlet fo > out.xml
+ret=$?
+
+if [ $ret -ne 0 ]
+then
+	echo "failed."
 	exit 1
 fi
 
-java mixxxml controller.js | xmlstarlet fo > out.xml
-
 cat out.xml
 
+echo ""
+echo "success!"
+echo ""
 echo "move out.xml to ../controllers/RMP-3.midi.xml ?"
 echo "copy controller.js to ../controllers/Reloop-RMP-3-scripts.js ?"
-echo "ctrl+c to abort"
+echo "enter to continue, ctrl+c to abort"
 read a
 
-mv out.xml ../controllers/RMP-3.midi.xml
-cp controller.js ../controllers/Reloop-RMP-3-scripts.js
+mv out.xml ../controllers/RMP-3.midi.xml \
+&& cp controller.js ../controllers/Reloop-RMP-3-scripts.js
 
+#EOF
