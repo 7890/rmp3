e5ffbba7563cbb5399e2f8fe5e2a90c2d1c54f31
Thomas Brand <tom@trellis.ch>
2018-07-04 18:53:27 +0200

    add variables: MIXXX_CONTROLLER_INSTALL_DIR, CONTROLLER_XML_FILENAME, CONTROLLER_JS_FILENAME. change resulting filenames to match mixxx format: 'company model.midi.xml' and 'company-model-scripts.js'. optional install of resulting files to MIXXX_CONTROLLER_INSTALL_DIR.

--- src/dist.sh
+++ src/dist.sh
@@ -4,6 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #run in src directory
 cd "$DIR"
 
+MIXXX_CONTROLLER_INSTALL_DIR="/usr/share/mixxx/midi"
+CONTROLLER_XML_FILENAME="Reloop RMP-3.midi.xml"
+CONTROLLER_JS_FILENAME="Reloop-RMP-3-scripts.js"
+
 checkAvail()
 {
 	which "$1" >/dev/null 2>&1
@@ -15,9 +19,15 @@ checkAvail()
 	fi
 }
 
-
 for tool in java javac xmlstarlet mktemp; \
 	do checkAvail "$tool"; done
+
+if [ ! -d "$MIXXX_CONTROLLER_INSTALL_DIR" ]
+then
+	echo -e "install directory not found: $MIXXX_CONTROLLER_INSTALL_DIR"
+	echo -e "aborting."
+	exit -1
+fi
 
 if [ ! -r "mixxxml.class" ]
 then
@@ -43,12 +53,24 @@ cat "$tmpfile"
 echo ""
 echo "success!"
 echo ""
-echo "copy resulting XML file to ../controllers/RMP-3.midi.xml ?"
-echo "copy controller.js to ../controllers/Reloop-RMP-3-scripts.js ?"
+echo "copy resulting XML file to ${DIR}/../${CONTROLLER_XML_FILENAME} ?"
+echo "copy controller.js to ${DIR}/../$CONTROLLER_JS_FILENAME ?"
 echo "enter to continue, ctrl+c to abort"
 read a
 
-mv "$tmpfile" ../controllers/RMP-3.midi.xml \
-&& cp controller.js ../controllers/Reloop-RMP-3-scripts.js
+mv "$tmpfile" "${DIR}/../${CONTROLLER_XML_FILENAME}" \
+&& cp controller.js "${DIR}/../$CONTROLLER_JS_FILENAME" \
+&& chmod 644 "${DIR}/../${CONTROLLER_XML_FILENAME}" \
+&& chmod 644 "${DIR}/../${CONTROLLER_JS_FILENAME}"
+
+echo "install to ${MIXXX_CONTROLLER_INSTALL_DIR}?"
+read a
+sudo -k
+sudo cp "${DIR}/../${CONTROLLER_XML_FILENAME}" "${MIXXX_CONTROLLER_INSTALL_DIR}" \
+&& sudo cp "${DIR}/../${CONTROLLER_JS_FILENAME}" "${MIXXX_CONTROLLER_INSTALL_DIR}" \
+&& sudo chmod 644 "${MIXXX_CONTROLLER_INSTALL_DIR}/${CONTROLLER_XML_FILENAME}" \
+&& sudo chmod 644 "${MIXXX_CONTROLLER_INSTALL_DIR}/${CONTROLLER_JS_FILENAME}"
+
+echo "done"
 
 #EOF
